76f5278f502c7b0a9647b57838625e9634a5a80e
marykasp <kasparian.mary@gmail.com>
2023-09-02 16:21:31 -0500

    style: add styling to sponsors section on home page

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -66,9 +66,9 @@ const Home = ({ setSelectedPage }: Props) => {
 
       {/* Sponsors - only show on desktop */}
       {isAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
+        <div className="h-[150px] w-full bg-gray-50 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
               <img
                 src={SponsorRedBull}
                 alt="redbull-sponsor"
